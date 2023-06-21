a3180c1792fd92cb0fa810a21454ce71565950f7
Julian <jumpingafterrain@gmail.com>
2023-06-21 22:15:33 +0200

    Initial Commit

---
+++ editr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  editr
+//
+//  Created by Julian ranieri on 6/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ editr/editrApp.swift
@@ -0,0 +1,17 @@
+//
+//  editrApp.swift
+//  editr
+//
+//  Created by Julian ranieri on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct editrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
